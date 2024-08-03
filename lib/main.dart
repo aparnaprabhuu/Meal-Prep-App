f0ad51bb2b2c4dacd49b4c5626361fa13753ef06
aparnaprabhuu <aparnaprabhu83599@gmail.com>
2024-08-03 20:30:01 +0530

    Adding all files

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: theme,
       home: const TabsScreen(),
     );
